421c3ed9d13d8d24f23cf47e81dac306ae054f4b
Keigo77 <147687878+Keigo77@users.noreply.github.com>
2025-06-13 14:28:34 +0900

    feat: ライフ制を実装

--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -5,10 +5,26 @@ using UnityEngine.UI;
 
 public class GameDirector : MonoBehaviour
 {
-    public Image hpGaugeImage;
+    // public Image hpGaugeImage;
+    public int life = 3;
+    public GameObject heartImage1;
+    public GameObject heartImage2;
+    public GameObject heartImage3;
 
     public void DecreaseHP()
     {
-        hpGaugeImage.fillAmount -= 0.1f;
+        life--;
+        if (life == 2)
+        {
+            heartImage3.SetActive(false);
+        } 
+        else if (life == 1)
+        {
+            heartImage2.SetActive(false);
+        } 
+        else if (life == 0)
+        {
+            heartImage1.SetActive(false);
+        }
     }
 }
